2b8b0376d1bd4b7e26167c09d0502d2125fc504e
Akash Ravi <ravi48@purdue.edu>
2021-11-06 13:05:18 -0900

    Fixes compile error for test.c

--- test.checked.c
+++ test.checked.c
@@ -23,7 +23,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "cJSON.h"
+#include "cJSON.checked.h"
 
 /* Used by some code below as an example datatype. */
 struct record
